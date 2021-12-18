ea532ee2ce0787024402605b6e6e8103bbe655f4
White Devil <94506157+OsharaShaveen@users.noreply.github.com>
2021-12-18 11:21:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import  os
+
+BOT_TOKEN = os.environ.get("5098802636:AAFdtaxILfiplDmAh6rPNvbiB9k6vB4RyaQ")
+APP_ID = int(os.environ.get("5367954"))
+API_HASH = os.environ.get("f2e9b124aa5d365cc86658f8b77899a3")
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
